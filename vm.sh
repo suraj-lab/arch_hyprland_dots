7ebd2e0b52ba1cf98470de582d076595bc0389ec
suraj-lab <surajsingh1337@gmail.com>
2023-12-06 21:57:07 +0000

    Updated to use to loop to delete /dev/shm/looking-glass when VM is not running and relaunch looking-glass-client when it is

--- vm.sh
+++ vm.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-virsh --connect qemu:///system start Gaming
-sleep 2
-looking-glass-client -m 97 &
-exit
+
+tmp=$(virsh --connect qemu:///system list  | grep " Gaming " | awk '{ print $3}')
+
+if ([ "x$tmp" == "x" ] || [ "x$tmp" != "xrunning" ] || ! [ -f /dev/shm/looking-glass ]);
+then
+	virsh --connect qemu:///system start Gaming
+	sleep 2
+	looking-glass-client -m 97 &
+	
+elif ([ "x$tmp" = "running" ] || [ -f /dev/shm/looking-glass ]);
+then
+	looking-glass-client -m 97 &
+fi
+
+end
